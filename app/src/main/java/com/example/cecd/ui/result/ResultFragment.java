ee2b89f2802b3939734b0386ac63821acc374977
joohun484 <joohun484@naver.com>
2019-11-22 16:48:49 +0900

    Auto Save Function Update

--- app/src/main/java/com/example/cecd/ui/result/ResultFragment.java
+++ app/src/main/java/com/example/cecd/ui/result/ResultFragment.java
@@ -1,6 +1,9 @@
 package com.example.cecd.ui.result;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +15,11 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.cecd.R;
 import com.example.cecd.ui.SharedViewModel;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ResultFragment extends Fragment {
 
@@ -26,6 +34,36 @@ public class ResultFragment extends Fragment {
         final ImageView imgView = root.findViewById(R.id.img_notification);
         imgView.setImageBitmap(SharedViewModel.bitmap);
 
+        if(isExternalStorageWritable()) {
+            try {
+                File file = new File(SharedViewModel.getPath().substring(0, SharedViewModel.getPath().length()-4)+"_out.png");
+                file.createNewFile();
+                file.setWritable(true);
+                if (file.exists()) file.delete();
+                FileOutputStream out = new FileOutputStream(file.getAbsolutePath());
+                SharedViewModel.bitmap.compress(Bitmap.CompressFormat.PNG, 100, out); // bmp is your Bitmap instance
+                out.close();
+                // PNG is a lossless format, the compression factor (100) is ignored
+                Log.e("Bitmap Saved!!", file.getAbsolutePath());
+            } catch (FileNotFoundException e) {
+                Log.e("Bitmap File Not Found Error: ", e.getMessage());
+                e.printStackTrace();
+            } catch (IOException e) {
+                Log.e("Bitmap IO Error: ", e.getMessage());
+                e.printStackTrace();
+            }
+        }else{
+            Log.e("External Storage Unavailiable","");
+        }
+
         return root;
+    }
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
     }
 }
